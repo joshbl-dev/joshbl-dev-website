38472447839004e47f9c3a48406c079d507908c2
Josh Landsman <jlandsman@gatech.edu>
2022-07-25 13:47:31 -0400

    custom error pages

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,1 +1,16 @@
-export { default } from "next/error";
+import PageTemplate from "../components/PageTemplate";
+
+import { ErrorProps } from "next/error";
+
+Error.getInitialProps = ({ res, err }) => {
+	const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+	return { statusCode };
+};
+
+export default function Error({ statusCode }: ErrorProps) {
+	return (
+		<PageTemplate
+			page={{ title: "Error", active: true, url: `${statusCode}` }}
+			components={<h1>{statusCode} - Error</h1>} />);
+}
+
