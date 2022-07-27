6279817c437eeb0d3afb5e7a8bb615f4db57894c
Josh Landsman <jlandsman@gatech.edu>
2022-07-26 22:56:17 -0400

    PokeWars Description

--- types/constants.ts
+++ types/constants.ts
@@ -1,10 +1,10 @@
-import axios, {AxiosInstance} from "axios";
-import {BaseUrl} from "./BaseUrl";
-import {PageInfo} from "./PageInfo";
-import {ProjectInfo} from "./ProjectInfo";
+import axios, { AxiosInstance } from "axios";
+import { BaseUrl } from "./BaseUrl";
+import { PageInfo } from "./PageInfo";
+import { ProjectInfo } from "./ProjectInfo";
 
 export const AXIOS_INSTANCE: AxiosInstance = axios.create({
-	baseURL: BaseUrl.JOSHBL_API,
+	baseURL: BaseUrl.JOSHBL_API
 });
 
 
@@ -12,23 +12,23 @@ export const pages: PageInfo[] = [
 	{
 		title: "Home",
 		active: true,
-		url: "/",
+		url: "/"
 	},
 	{
 		title: "Projects",
 		active: true,
-		url: "/projects",
+		url: "/projects"
 	},
 	{
 		title: "Bio",
 		active: false,
-		url: "/bio",
+		url: "/bio"
 	},
 	{
 		title: "???",
 		active: false,
-		url: "/mystery",
-	},
+		url: "/mystery"
+	}
 ];
 
 export const projects: ProjectInfo[] = [
@@ -38,7 +38,7 @@ export const projects: ProjectInfo[] = [
 		url: "https://github.com/joshbl-dev/joshbl-dev-website",
 		mediaType: "image",
 		mediaContent: "/images/projects/next.webp",
-		altText: "Next.js Logo",
+		altText: "Next.js Logo"
 	},
 	{
 		title: "NestJS Personal API",
@@ -46,25 +46,24 @@ export const projects: ProjectInfo[] = [
 		url: "https://github.com/joshbl-dev/joshbl-dev-api",
 		mediaType: "image",
 		mediaContent: "/images/projects/nest.webp",
-		altText: "NestJS Logo",
+		altText: "NestJS Logo"
 	},
 	{
 		title: "NCR Global Hackathon",
 		description: "Created a production REST API for NCR’s Global Hackathon to organize international participants, teams, and " +
-			"conference talks using NestJS, Azure, and PostgreSQL",
+			"conference talks using NestJS, Azure, and PostgreSQL.",
 		url: "https://hackathon.ncr.com/",
 		mediaType: "image",
 		mediaContent: "/images/projects/ghu.webp",
-		altText: "Global Hackathon Site",
+		altText: "Global Hackathon Site"
 	},
 	{
 		title: "PokéWars - Minecraft Server",
-		description: "An Android application in Java for the 2020 QuackHacks hackathon, which provided a platform " +
-			"for individuals to exchange cooking recipes in a social media style using a Google Firebase real-time database.",
+		description: "Attack and Defend mini-game played by thousands where the players become a Pokémon with custom abilities. Developed in Java using Minecraft API.",
 		url: "https://www.pokefind.co/",
 		mediaType: "video",
 		mediaContent: "https://www.youtube.com/embed/3bjznlsCYQ8",
-		altText: "PokeWars Video Demo",
+		altText: "PokeWars Video Demo"
 	},
 	{
 		title: "PKNK - Food Social Media",
@@ -73,7 +72,7 @@ export const projects: ProjectInfo[] = [
 		url: "https://github.com/joshbl-dev/PKNK",
 		mediaType: "video",
 		mediaContent: "https://www.youtube.com/embed/h2Yw2n202WM",
-		altText: "PKNK Video Demo",
+		altText: "PKNK Video Demo"
 	},
 	{
 		title: "Project Gamma Alpha",
@@ -81,6 +80,6 @@ export const projects: ProjectInfo[] = [
 		url: "https://github.com/joshbl-dev/Project-Gamma",
 		mediaType: "image",
 		mediaContent: "/images/projects/pga.webp",
-		altText: "Project Gamma Alpha Demo",
-	},
+		altText: "Project Gamma Alpha Demo"
+	}
 ];
