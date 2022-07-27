2258130f45c45deb87b2eed95f9a3a418ee780e9
Josh Landsman <jlandsman@gatech.edu>
2022-07-27 00:00:43 -0400

    Update ProjectCard.tsx

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -98,6 +98,7 @@ export default function ProjectCard(props: ProjectCardProps) {
 					<Box
 						minHeight={"200px"}
 						height={"fit-content"}
+						minWidth={"100%"}
 						alignItems={"center"}
 					>
 						{props.media}
