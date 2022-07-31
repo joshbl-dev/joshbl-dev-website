9da87f1a089e525d8ca86c7f14152ee172dfe1bb
Josh Landsman <jlandsman@gatech.edu>
2022-07-31 15:22:06 -0400

    nav bar possible fix

--- components/TopicChip.tsx
+++ components/TopicChip.tsx
@@ -1,4 +1,4 @@
-import {Chip} from "@mui/material";
+import { Chip, Typography } from "@mui/material";
 
 type topicChipProps = {
 	text: string;
@@ -8,16 +8,26 @@ export default function TopicChip(props: topicChipProps) {
 	const text = props.text;
 
 	return (
-		<Chip color={"secondary"} key={text + Math.random()} label={text}
+		<Chip color={"secondary"} label={
+			<Typography
+				fontWeight={"bold"}
+				fontSize={"1rem"}
+				style={{ whiteSpace: "pre-line" }}>{text}</Typography>
+		}
 			// Todo: Redirect to external link
 			// clickable={true}
 			  sx={{
+				  // height: "fit-content",
+				  padding: .5,
+				  minHeight: "fit-content",
+				  minWidth: "fit-content",
+				  maxWidth: "fit-content",
+				  // textAlign: "center",
 				  marginRight: .5,
 				  marginLeft: .5,
 				  marginBottom: 1,
-				  marginTop: 1,
-				  fontWeight: "bold",
-				  fontSize: "1rem",
+				  marginTop: 1
+
 			  }} />
 	);
 
--- pages/bio/index.tsx
+++ pages/bio/index.tsx
@@ -1,12 +1,12 @@
-import {pages} from "../../types/constants";
+import { pages } from "../../types/constants";
 import PageTemplate from "../../components/PageTemplate";
-import {Box, Divider, Typography} from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import Timeline from "../../components/Timeline";
 import {
 	desktop_hide,
 	desktop_show,
 	tablet_hide,
-	tablet_show,
+	tablet_show
 } from "../../styles/theme";
 import TopicChip from "../../components/TopicChip";
 import ImageCarousel from "../../components/ImageCarousel";
@@ -22,7 +22,8 @@ function facts(subject: string, facts: string[]) {
 						variant={"h5"}>{subject}:
 				<Box display="block">
 
-					{facts.map(fact => <TopicChip text={fact} />)}
+					{facts.map(fact => <TopicChip key={fact + Math.random()}
+												  text={fact} />)}
 				</Box>
 			</Typography>
 		</Box>);
@@ -38,7 +39,7 @@ function divider() {
 						 // padding: "1px",
 						 width: "75%",
 						 borderWidth: "5px",
-						 borderRadius: "20px",
+						 borderRadius: "20px"
 					 }} />
 		</Box>);
 }
@@ -51,75 +52,86 @@ const csCourses: string[] = ["Object-Oriented Programming", "Data Structures & A
 	"Systems and Networks", "Intro to AI"];
 const mathCourses: string[] = ["Applied Combinatorics", "Linear Algebra", "Discrete Mathematics"];
 
+
+function content() {
+	return (
+		<>
+			{sectionTitle("College Education")}
+			<Box border={"solid"} borderRadius={"20px"} minWidth={"lg"}
+				 maxWidth={"100%"} margin={"20px"}>
+
+				<Box justifyContent={"space-between"}
+					 {...desktop_show("flex")}>
+					<Typography
+						margin={"20px"} variant={"h4"}
+						whiteSpace={"nowrap"}>
+						Georgia Institute of Technology
+					</Typography>
+
+					<Typography margin={"20px"}
+								whiteSpace={"nowrap"}
+								variant={"h4"}>
+						2020-2024
+					</Typography>
+				</Box>
+
+				<Box
+					height={"fit-content"}
+					justifyContent={"center"}
+					{...desktop_hide("block")}>
+					<Typography
+						{...tablet_hide("block")}
+						margin={"20px"}
+						variant={"h4"}
+						text-align={"center"}
+						whiteSpace={"nowrap"}>
+						Georgia Tech
+					</Typography>
+					<Typography
+						{...tablet_show("block")}
+						margin={"20px"}
+						variant={"h4"}
+						text-align={"center"}
+						whiteSpace={"nowrap"}>
+						Georgia Institute of Technology
+					</Typography>
+
+					<Typography margin={"20px"}
+								whiteSpace={"nowrap"}
+								variant={"h5"}>
+						2020-2024
+					</Typography>
+				</Box>
+
+
+				<Box padding={"10px"}>
+					{facts("Major", ["Computer Science"])}
+					{facts("Concentrations", ["Intelligence", "Information Internetworks"])}
+					{facts("CS Courses", csCourses)}
+					{facts("Math Courses", mathCourses)}
+				</Box>
+			</Box>
+			{divider()}
+			{sectionTitle("Professional Work Experience")}
+			{Timeline()}
+
+			{divider()}
+			{sectionTitle("Interests")}
+			{ImageCarousel()}
+		</>
+	);
+}
+
 export default function Bio() {
 	return (
-		<PageTemplate page={page} components={<>
-			<Box margin={"20px"} justifyContent={"center"}
-				 textAlign={"center"}>
-				{sectionTitle("College Education")}
-				<Box border={"solid"} borderRadius={"20px"} minWidth={"lg"}
-					 maxWidth={"100%"} margin={"20px"}>
-
-					<Box justifyContent={"space-between"}
-						 {...desktop_show("flex")}>
-						<Typography
-							margin={"20px"} variant={"h4"}
-							whiteSpace={"nowrap"}>
-							Georgia Institute of Technology
-						</Typography>
-
-						<Typography margin={"20px"}
-									whiteSpace={"nowrap"}
-									variant={"h4"}>
-							2020-2024
-						</Typography>
-					</Box>
-
-					<Box
-						height={"fit-content"}
-						justifyContent={"center"}
-						{...desktop_hide("block")}>
-						<Typography
-							{...tablet_hide("block")}
-							margin={"20px"}
-							variant={"h4"}
-							text-align={"center"}
-							whiteSpace={"nowrap"}>
-							Georgia Tech
-						</Typography>
-						<Typography
-							{...tablet_show("block")}
-							margin={"20px"}
-							variant={"h4"}
-							text-align={"center"}
-							whiteSpace={"nowrap"}>
-							Georgia Institute of Technology
-						</Typography>
-
-						<Typography margin={"20px"}
-									whiteSpace={"nowrap"}
-									variant={"h5"}>
-							2020-2024
-						</Typography>
-					</Box>
-
-
-					<Box padding={"10px"}>
-						{facts("Major", ["Computer Science"])}
-						{facts("Concentrations", ["Intelligence", "Information Internetworks"])}
-						{facts("CS Courses", csCourses)}
-						{facts("Math Courses", mathCourses)}
-					</Box>
+		<PageTemplate page={page} components={
+			<>
+				<Box maxWidth="100vw" margin={"20px"}
+					 justifyContent={"center"}
+					 textAlign={"center"}>
+					{content()}
 				</Box>
-				{divider()}
-				{sectionTitle("Professional Work Experience")}
-				{Timeline()}
-
-				{divider()}
-				{sectionTitle("Interests")}
-				{ImageCarousel()}
-			</Box>
-		</>} />
+			</>} />
 	);
 };
 
